cff4d676b739be60c3ac19f62488e6ad48fe2c04
perokvist <per_okvist@live.se>
2013-10-09 15:11:51 +0200

    Comments

--- Jayway.Throttling/IThrottlingService.cs
+++ Jayway.Throttling/IThrottlingService.cs
@@ -4,6 +4,16 @@ namespace Jayway.Throttling
 {
     public interface IThrottlingService
     {
+        /// <summary>
+        /// Decrease credits and check if the account is throttled.
+        /// </summary>
+        /// <param name="account">the unique id of the customer account</param>
+        /// <param name="cost">the cost of this particular call</param>
+        /// <param name="intervalFactory">callback to get the initial settings for an interval. In a
+        /// real application it might take some time to get this value and
+        /// therefore we should not do this on every request.</param>
+        /// <returns>true if the request should be allowed, false if the account
+        /// should be throttled</returns>
         bool Allow(string account, long cost, Func<Interval> intervalFactory);
     }
 }
